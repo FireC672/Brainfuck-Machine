4f46935a039a95e61da2c2b03d29237bcae8bf5d
FireC657 <comptagedepoints@gmail.com>
2023-07-14 00:18:36 +0100

    The prototype of the lexical node

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -1,4 +1,21 @@
 #ifndef LEXER_INCLUDED
 #define LEXER_INCLUDED
 
+#include <string>
+#include <vector>
+
+/// @brief Contains the data of the lexical data.
+struct lexical_node {
+    // 
+    std::vector<std::string> lexical_strs;
+
+    lexical_node(const std::vector<std::string>& lexstr){
+        lexical_strs = lexstr;
+    }
+
+    ~lexical_node(){
+        lexical_strs.clear();
+    }
+};
+
 #endif
