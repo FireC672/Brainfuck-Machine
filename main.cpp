c9eeee576cfe1644bed97fdfe8f2257d9210c66e
FireC657 <comptagedepoints@gmail.com>
2023-07-17 16:16:30 +0100

    Argument error handling

--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 #include "include/parser.hpp"
 
 int main(int argc, char **argv){
+    
+    if(argc < 2){
+        std::cerr << "\033[1m\033[31merror:\033[0m no source file.\n";
+        return 1;
+    }
+
     std::ifstream source; 
     std::ofstream produced;
 
     source.open(argv[1], std::ios_base::in);
-    produced.open(argv[2], std::ios_base::out);
+    if(!(argc < 3)){
+       produced.open(argv[2], std::ios_base::out);
+    }else {
+       produced.open(strcat(argv[1], "out.bf"), std::ios_base::out);
+    }
 
     std::string content; 
     while(!source.eof()){
