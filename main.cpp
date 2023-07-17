7b72b118ec673595e159be36e858253ec45396a0
FireC657 <comptagedepoints@gmail.com>
2023-07-17 16:13:48 +0100

    Everything done

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "include/lexer.hpp"
 #include "include/parser.hpp"
 
-// +.[+.]
-int main(){
-    lexical_tree *t = analyzeSource("sup $01a\nout $01\npsh\nsup $01\nout $01\npop\n");
-    lexical_tree *ct = concretiztaion(t);
-    std::string outfuck = alterBrainfuck(ct);
-    std::cout << outfuck;
-    delete t;
-    delete ct;
+int main(int argc, char **argv){
+    std::ifstream source; 
+    std::ofstream produced;
+
+    source.open(argv[1], std::ios_base::in);
+    produced.open(argv[2], std::ios_base::out);
+
+    std::string content; 
+    while(!source.eof()){
+        content.push_back(source.get());
+    }
+    
+    lexical_tree *abstractTree = analyzeSource(content.c_str());
+    lexical_tree *concretTree = concretiztaion(abstractTree);
+    std::string translatedSource = alterBrainfuck(concretTree);
+
+    delete abstractTree, concretTree;
+
+    produced << translatedSource;
+
+    source.close();
+    produced.close();
     return 0;
 }
